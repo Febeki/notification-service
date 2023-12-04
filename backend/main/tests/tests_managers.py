29241c81fe76aed2f90ebf091049f9ff80d81647
Denis Gudkov <gudkov.den.5454@gmail.com>
2023-12-04 18:21:28 +0500

    test 28

--- backend/main/tests/tests_managers.py
+++ backend/main/tests/tests_managers.py
@@ -16,4 +16,4 @@ class UserManagerTest(TestCase):
         user = User.objects.create_superuser(username='test', password='password1234')
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
-        self.assertFalse(user.is_superuser)
+        self.assertTrue(user.is_superuser)
